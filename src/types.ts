5244df8c1799e3d024d75f4b45ea278f9f4e95bc
InSeong-So <goflvhxj2547@gmail.com>
2023-08-05 02:24:23 +0900

    feat: 앱 전역에서 사용될 리액트 네이티브 타입들을 추가합니다

---
+++ src/types.ts
@@ -0,0 +1,52 @@
+/**
+ * Learn more about using TypeScript with React Navigation:
+ * https://reactnavigation.org/docs/typescript/
+ */
+
+import { RouteProp } from '@react-navigation/native';
+import type { ParamListBase, NavigatorScreenParams } from '@react-navigation/native';
+import type { StackScreenProps } from '@react-navigation/stack';
+
+declare global {
+  namespace ReactNavigation {
+    interface RootParamList extends RootStackParamList {}
+  }
+}
+
+export type RootStackParamList = {
+  Drawer: NavigatorScreenParams<RootTabParamList> | undefined;
+  Detail: undefined;
+  Modal: undefined;
+  NotFound: undefined;
+};
+
+export type RootStackScreenProps<Screen extends keyof RootStackParamList> = StackScreenProps<
+  RootStackParamList,
+  Screen
+>;
+
+export type RootTabParamList = {
+  Home: undefined;
+  Settings: undefined;
+};
+
+export type CommonScreenProps = StackScreenProps<ParamListBase, string> & {
+  params?: Record<string, string>;
+};
+
+export type WebViewRoute = RouteProp<ParamListBase, string> & { params?: Record<string, string> };
+
+export type NativeEventParams =
+  | {
+      type: 'USER_AUTH';
+      token: string | null;
+    }
+  | {
+      type: 'ROUTER_EVENT';
+      path: string;
+      screenName: string;
+      data: Record<string, unknown>;
+    }
+  | {
+      type: 'ROUTER_REFRESH' | 'ROUTER_BACK';
+    };
